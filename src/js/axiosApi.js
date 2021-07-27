cf715a81606122f98ee1a68b1271837e13e99e7b
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-07-27 23:21:50 +0300

    done pagination

--- src/js/axiosApi.js
+++ src/js/axiosApi.js
@@ -4,39 +4,49 @@ import Notiflix from 'notiflix';
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 1000;
 
-const { input, button, buttonMore, gallery } = refs;
+const { form, input, button, buttonMore, gallery } = refs;
 const baseUrl = 'https://pixabay.com/api/';
 const apiKey = '?key=22659093-928fc585fa86297f1703a77f0';
 const params = '&image_type=photo&orientation=horizontal&safesearch=true';
 
-// const myUrl = baseUrl + apiKey + `&q=${evt}` + params;
+let page = 1;
+let query = '';
+let photos = [];
+const per_page = '&per_page=40';
 
-input.addEventListener('input', debounce(getImages, DEBOUNCE_DELAY));
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  gallery.innerHTML = '';
+  query = input.value;
+  getImages();
+  form.reset();
+  buttonMore.classList.remove('hidden');
+});
 
-function getImages(evt) {
-  console.log(evt.target.value);
-  axios
-    .get(baseUrl + apiKey + `&q=${evt.target.value}` + params)
-    .then(response => {
-      return response.data;
-    })
-    .then(data => {
-      if (data.hits.length !== 0) {
-        return data.hits;
-      }
-    })
-    .then(photos => {
-      const fechedPhotos = photos
-        .map(photo => {
-          console.log(photo);
-          return `<div class="photo-card"><div class="photo-thumb"><img src=${photo.webformatURL} alt=${photo.webformatURL} loading="lazy" /></div><div class="info"><p class="info-item"><b>Likes</b><br />${photo.likes}</p><p class="info-item"><b>Views</b><br />${photo.views}</p><p class="info-item"><b>Comments</b><br />${photo.comments}</p><p class="info-item"><b>Downloads</b><br />${photo.downloads}</p></div></div>`;
-        })
-        .join('');
-      gallery.innerHTML = fechedPhotos;
-    })
-    .catch(() => {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.',
-      );
-    });
+async function getImages() {
+  let url = baseUrl + apiKey + `&q=${query}` + params + `&page=${page}` + per_page;
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    if (data.hits.length !== 0) {
+      photos = data.hits;
+    }
+    const fechedPhotos = photos
+      .map(photo => {
+        return `<div class="photo-card"><img src=${photo.webformatURL} alt=${photo.tags} loading="lazy" /><div class="info"><p class="info-item"><b>Likes</b><br />${photo.likes}</p><p class="info-item"><b>Views</b><br />${photo.views}</p><p class="info-item"><b>Comments</b><br />${photo.comments}</p><p class="info-item"><b>Downloads</b><br />${photo.downloads}</p></div></div>`;
+      })
+      .join('');
+    gallery.insertAdjacentHTML('beforeend', fechedPhotos);
+    buttonMore.addEventListener('click', onLoadMore);
+  } catch (error) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  }
+}
+
+function onLoadMore() {
+  page += 1;
+
+  getImages();
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,7 @@
 export default {
+  form: document.getElementById('search-form'),
   input: document.querySelector('input'),
-  button: document.querySelector('form.button'),
+  button: document.querySelector('button'),
   buttonMore: document.querySelector('.load-more'),
   gallery: document.querySelector('.gallery'),
 };
